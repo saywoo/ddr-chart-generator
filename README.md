0c0ccbef010ee9a174811d40460cef7ca4b240d1
saywoo <saywoo5121@gmail.com>
2024-08-08 23:53:24 +0900

    a

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # maimai-chart-generator
 아케이드 리듬게임 마이마이의 레이팅 차트 이미지를 생성하는 툴
+
+
+## 메모
+
+javascript:(function(d,s){s=d.createElement('script');s.src='/Users/saywoo/Documents/maimai-chart-generator/test.js?'+Date.now();d.getElementsByTagName('head')[0].appendChild(s);})(document);
--- test.js
+++ test.js
@@ -26,5 +26,8 @@
 		return fetchHTML(url, BASE_URL + FLARE_SINGLE_PATH);
 	};
 
-    console(getSkillData)
+    console.log(getSkillData);
 })
+
+
+javascript:(function(){var d=document;var url='https://saywoo.github.io/maimai-chart-generator/test.js';var e=d.createElement('script');e.src=url;d.getElementsByTagName('head')[0].appendChild(e);})()
