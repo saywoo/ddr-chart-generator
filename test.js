8e1c135189740cb315a096fc83b545969d4a039c
saywoo <saywoo5121@gmail.com>
2024-08-11 04:14:34 +0900

    a

--- test.js
+++ test.js
@@ -20,11 +20,22 @@
 
             let songName = songInfo[0].querySelector("a").textContent;
             let songImg = songInfo[0].querySelector("img").src;
-            songImg.replace(/kind=2/, "kind=1");
+            // 작은 이미지에서 큰 이미지로 변경
+            songImg.replace("kind=2", "kind=1");
 
+            let tmp = songInfo[1].innerHTML.split("<br>");
+            let songDiff = tmp[0];
+            let songLv = tmp[1];
+            
+            let songFlareLv = songInfo[2].querySelector("img").src;
+            let songFlareSkill = songInfo[3].textContent;
+
+            console.log(songName);
             console.log(songImg);
-
-            // console.log(matches[i]);
+            console.log(songDiff);
+            console.log(songLv);
+            console.log(songFlareLv);
+            console.log(songFlareSkill);
         }
     }
 
