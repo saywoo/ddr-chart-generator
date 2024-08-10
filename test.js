bdfa1fe8f20799031621cfae0afd7300f01fbf55
saywoo <saywoo5121@gmail.com>
2024-08-11 03:04:22 +0900

    a

--- test.js
+++ test.js
@@ -2,18 +2,12 @@
 	const BASE_URL = "https://p.eagate.573.jp/game/ddr/ddrworld/playdata/";
     const FLARE_SINGLE_PATH = "flare_data_single.html";
     const FLARE_DOUBLE_PATH = "flare_data_double.html";
-	const LIMIT = 150;
 
-	const fetchHTML = async (url) => {
-        return new DOMParser().parseFromString(await (await fetch(url)).text(), "text/html");
-	};
-
-	const getSingleData = async (page) => {
-		return fetchHTML(page);
+	async function getSingleData(url) {
+		return new DOMParser().parseFromString(await (await fetch(url)).text(), "text/html");
 	};
 
     const dataPage = await getSingleData(BASE_URL + FLARE_SINGLE_PATH);
     console.log(dataPage);
-
 })();
 
