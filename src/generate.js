3424e2db4599034e187828aa5a6dd2f149730648
saywoo <saywoo5121@gmail.com>
2024-08-12 04:20:43 +0900

    2024.0812.0420

--- src/generate.js
+++ src/generate.js
@@ -13,7 +13,8 @@
 	};
 
     // [ classic, white, gold ] 곡 리스트
-    var songList = [[], [], []];
+    var singleSongList = [[], [], []];
+    var doubleSongList = [[], [], []];
 
     const dataPage = await fetchDoc(BASE_URL + FLARE_SINGLE_PATH);
 
@@ -39,11 +40,40 @@
             let songFlareLv = songInfo[2].querySelector("img").src;
             let songFlareSkill = songInfo[3].textContent;
 
-            songList[i].push({'name':songName, 'img':songImg, 'diff':songDiff, 'flareLv':songFlareLv, 'flareSkill':songFlareSkill});
+            singleSongList[i].push({'name':songName, 'img':songImg, 'diff':songDiff, 'flareLv':songFlareLv, 'flareSkill':songFlareSkill});
         }
     }
+    console.log(singleSongList);
 
-    console.log(songList);
+
+    dataPage = await fetchDoc(BASE_URL + FLARE_DOUBLE_PATH);
+
+    for (let i = 0; i < CATEGORY_TABLE.length; i++) {
+        let matches = dataPage.querySelectorAll(CATEGORY_TABLE[i]);
+
+        for (let j = 0; j < matches.length; j++) {
+            // 곡 정보를 파싱해서 songList에 저장
+            let songInfo = matches[j].querySelectorAll("td");
+
+            let songName = songInfo[0].querySelector("a").textContent;
+
+            if (songName.length >= MAX_NAME_LENGTH) {
+                songName = songName.slice(0, MAX_NAME_LENGTH) + "...";
+            }
+
+            let songImg = songInfo[0].querySelector("img").src;
+            songImg = songImg.replace("kind=2", "kind=1");      // 작은 이미지에서 큰 이미지로 변경
+
+            let tmp = songInfo[1].innerHTML.split("<br>");
+            let songDiff = tmp[0].slice(0, 1) + "SP " + tmp[1].slice(3);    // 'ESP 14' 형태로 가공
+            
+            let songFlareLv = songInfo[2].querySelector("img").src;
+            let songFlareSkill = songInfo[3].textContent;
+
+            doubleSongList[i].push({'name':songName, 'img':songImg, 'diff':songDiff, 'flareLv':songFlareLv, 'flareSkill':songFlareSkill});
+        }
+    }
+    console.log(doubleSongList);
 
 })();
 
